ed1a339b142d8492664965b738ad118998b182c9
Senvisage <senvisage@gmail.com>
2018-03-14 10:19:20 +0100

    Adding basic tests

---
+++ symfony/tests/Controller/HomeControllerTest.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Senvisage
+ * Date: 13/03/2018
+ * Time: 10:03
+ */
+class HomeControllerTest
+{
+
+}
---
+++ symfony/tests/Controller/RecetteControllerTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Senvisage
+ * Date: 13/03/2018
+ * Time: 15:24
+ */
+
+namespace App\Tests\Controller;
+
+
+class RecetteControllerTest
+{
+
+}
